bfb2fe475297d11c040e0ecae883c857b021c070
Mark Rushton <mark@modernfidelity.co.uk>
2016-05-12 11:22:16 +0100

    frontpage markup updates

--- src/client/app/components/frontpage/frontpage.service.spec.js
+++ src/client/app/components/frontpage/frontpage.service.spec.js
@@ -1,64 +1,16 @@
-/**
- *
- * FRONTPAGE COMPONENT
- *
- * @author
- *    Mark Rushton <mark@modernfidelity.co.uk>
- *
- * @class app.Frontpage
- *
- * @description Provides the frontpage functionality for the site
- *
- * @memberof app
- *
- */
+'use strict';
 
-(function() {
-  'use strict';
+describe('project.frontpage module', function() {
 
-  angular.module('project.frontpage', ['ngRoute'])
+  beforeEach(module('project.frontpage'));
 
-    .config(['$routeProvider', function($routeProvider) {
-      $routeProvider.when('/frontpage', {
-        pageTitle: 'AAT - Welcome',
-        templateUrl: 'components/frontpage/frontpage.tpl.html',
-        controller: 'FrontpageController',
-        controllerAs: 'vm',
-        access: {
-          requiresLogin: false,
-          roles: []
-        }
-      });
-    }])
+  describe('frontpage controller', function() {
 
-    .controller('FrontpageController', FrontpageController);
+    it('should ....', inject(function($controller) {
+      //spec body
+      var FrontpageController = $controller('FrontpageController');
+      expect(FrontpageController).toBeDefined();
+    }));
 
-  // Inject Deps
-  FrontpageController.$inject = [];
-
-  /**
-   *
-   * Controller
-   *
-   * @constructor
-   */
-  function FrontpageController() {
-
-    var vm = this;
-    vm.pageContent = {};
-    vm.isPageLoading = true;
-
-    //API.getPageById('frontpage', false)
-    //    .then(function(response) {
-    //        console.log('get it', response.data.data);
-    //        vm.pageContent = response.data.data;
-    //        //$timeout(function(){vm.isPageLoading = false;}, 1000);
-    //        vm.isPageLoading = false;
-    //    }).catch(function(err) {
-    //    vm.isPageLoading = false;
-    //    vm.pageContent = {'error': 500};
-    //});
-
-  }
-
-}());
+  });
+});
