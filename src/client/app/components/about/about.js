7d92ce6542aa2d68f13d5d5998214ac5353a29f5
David Breuer <dbreuer83@gmail.com>
2016-05-16 11:44:37 +0100

    About section dynamic spotlights

--- src/client/app/components/about/about.js
+++ src/client/app/components/about/about.js
@@ -48,17 +48,98 @@
     vm.pageContent = {};
     vm.isPageLoading = true;
 
-    //API.getPageById('frontpage', false)
-    //    .then(function(response) {
-    //        console.log('get it', response.data.data);
-    //        vm.pageContent = response.data.data;
-    //        //$timeout(function(){vm.isPageLoading = false;}, 1000);
-    //        vm.isPageLoading = false;
-    //    }).catch(function(err) {
-    //    vm.isPageLoading = false;
-    //    vm.pageContent = {'error': 500};
-    //});
-
+    vm.pageContent = {
+      'slideshow': [
+        {
+          'id': 1,
+          'title': 'AAT(SA) membership',
+          'text': 'Join a professional accountancy body that recognises your ' +
+          'achievements and provides you with a range of benefits.',
+          'image': {
+            'src': 'http://aatsa-web.s3-eu-west-1.amazonaws.com/sa-prod/s3fs-public/hero/lungile_mawonga_2.jpg',
+            'alt': ''
+          },
+          'button': {
+            'text': 'Find out more',
+            'url': '/membership/about'
+          }
+        },
+        {
+          'id': 2,
+          'title': 'AAT(SA) qualifications',
+          'text': 'The AAT(SA) qualification can enable your career in finance and accounting' +
+          ' and help you make a truly positive contribution to your business, department or unit.',
+          'image': {
+            'src': 'http://aatsa-web.s3-eu-west-1.amazonaws.com/sa-prod/s3fs-public/hero/ayton_namwaza_banda_1.jpg',
+            'alt': ''
+          },
+          'button': {
+            'text': 'Find out more',
+            'url': '/qualifications/accounting-qualification'
+          }
+        }
+      ],
+      'spotlights': [
+        {
+          'id': 1,
+          'title': 'What we do',
+          'text': 'We are a professional body dedicated to the education, development,' +
+          ' regulation and support of accounting technicians in South Africa.',
+          'image': {
+            'src': 'http://aatsa-web.s3-eu-west-1.amazonaws.com/sa-prod/s3fs-public/styles/' +
+            'landing_page_content_box/public/landing_pages/israel_mogobe.jpg?itok=vXbL54nk',
+            'alt': 'What we do'
+          },
+          'button': {
+            'text': 'Find out more',
+            'url': '/about/what-we-do'
+          }
+        },
+        {
+          'id': 2,
+          'title': 'Contact us',
+          'text': 'You can call or email us with queries or to request more information about what we do.',
+          'image': {
+            'src': 'http://aatsa-web.s3-eu-west-1.amazonaws.com/sa-prod/s3fs-public/styles/' +
+            'landing_page_content_box/public/landing_pages/ashgar_ali_hassim.jpg?itok=KQngkAag',
+            'alt': 'Contact us'
+          },
+          'button': {
+            'text': 'Contact us',
+            'url': '/about/contact-us'
+          }
+        },
+        {
+          'id': 3,
+          'title': 'AAT UK',
+          'text': 'Visit the Association of Accounting Technician\'s UK web site.',
+          'image': {
+            'src': 'http://aatsa-web.s3-eu-west-1.amazonaws.com/sa-prod/s3fs-public/styles/' +
+            'landing_page_content_box/public/landing_pages/edward_makwana_saica.jpg?itok=-35K2xUE',
+            'alt': 'AAT UK'
+          },
+          'button': {
+            'text': 'Visit the AAT UK website',
+            'url': 'http://www.aat.org.uk/'
+          }
+        },
+        {
+          'id': 4,
+          'title': 'Terms and conditions',
+          'text': 'Access to and use of AAT(SA)\'s website constitutes ' +
+          'acceptance of the terms and conditions listed here.',
+          'image': {
+            'src': 'http://aatsa-web.s3-eu-west-1.amazonaws.com/sa-prod/s3fs-public/styles/' +
+            'landing_page_content_box/public/landing_pages/sylvia_segone_2.jpg?itok=OaqGU7O3',
+            'alt': 'Ts & Cs'
+          },
+          'button': {
+            'text': 'Read our terms and conditions',
+            'url': 'http://sa.aws.aat.org.uk/about/terms-conditions'
+          }
+        }
+      ]
+    };
   }
 
 }());
