1d8b6e46fd09a93e9b54ea4cc645098d575702c5
Mark Rushton <mark@modernfidelity.co.uk>
2016-05-19 12:26:32 +0100

    tests started

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration
 // http://karma-runner.github.io/0.10/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     // base path, that will be used to resolve files and exclude
     basePath: '',
@@ -11,22 +11,55 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/bower_components/angular-route/angular-route.js',
+      'src/client/app/bower_components/angular/angular.js',
+      'src/client/app/bower_components/angular-mocks/angular-mocks.js',
+      'src/client/app/bower_components/angular-route/angular-route.js',
 
-      'app/app.js',
+      'src/client/app/app.js',
+      'src/client/app/components/**/*.js',
+      'src/client/app/services/**/*.js',
+      'src/client/app/shared/**/*.js',
 
-      'app/site/api/**/*.js',
-      'app/site/components/**/*.js',
-      'app/site/shared/**/*.js',
-
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
+//       'test/e2e/**/*.js',
+      'test/unit/**/*.js'
     ],
 
     // list of files / patterns to exclude
     exclude: [],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      // 'src/app/**/*.js': 'coverage',
+
+      'src/app/**/!(.spec).js': 'coverage'
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['dots', 'progress', 'junit', 'coverage'],
+
+    junitReporter: {
+      outputDir: 'build/logs',
+      outputFile: 'junit.xml'
+    },
+
+    coverageReporter: {
+      dir: 'build/logs',
+      reporters: [
+        {
+          type: 'html',
+          dir: 'report-html/'
+        },
+        {
+          type: 'cobertura',
+          file: 'coverage.xml'
+        }
+
+      ]
+
+    },
 
     // web server port
     port: 8080,
@@ -35,10 +68,8 @@ module.exports = function (config) {
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
 
     // Start these browsers, currently available:
     // - Chrome
