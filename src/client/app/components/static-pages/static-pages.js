f01d3d80b81200dc763d6cfddbc351e023a4d1c7
David Breuer <dbreuer83@gmail.com>
2016-05-23 18:48:34 +0100

    Replaced social links in  the footer

--- src/client/app/components/static-pages/static-pages.js
+++ src/client/app/components/static-pages/static-pages.js
@@ -22,68 +22,10 @@
     .config(['$routeProvider', '$locationProvider',
       function($routeProvider, $locationProvider) {
 
-        $routeProvider.when('/about', {
-          title: 'About',
-          metaDescription: 'Information about the AAT',
-          templateUrl: 'components/static-pages/about.tpl.html',
-          access: {
-            requiresLogin: false,
-            roles: []
-          }
-        });
-
-        //AAT privacy policy
-        $routeProvider.when('/get-myaat/privacy-policy', {
-          pageTitle: 'AAT(SA) privacy policy',
-          templateUrl: 'components/static-pages/privacy.tpl.html',
-          controller: 'MyaatController',
-          controllerAs: 'vm',
-          access: {
-            requiresLogin: false,
-            roles: []
-          }
-        });
-
-        //AAT cookie policy
-        $routeProvider.when('/get-myaat/aat-cookie-policy', {
-          pageTitle: 'AAT cookie policy',
-          templateUrl: 'components/static-pages/cookie.tpl.html',
-          controller: 'MyaatController',
-          controllerAs: 'vm',
-          access: {
-            requiresLogin: false,
-            roles: []
-          }
-        });
-
         //Equality of opportunity
         $routeProvider.when('/get-myaat/aat-equal-opportunities-policy', {
           pageTitle: 'Equality of opportunity',
           templateUrl: 'components/static-pages/equal.tpl.html',
-          controller: 'MyaatController',
-          controllerAs: 'vm',
-          access: {
-            requiresLogin: false,
-            roles: []
-          }
-        });
-
-        //Terms and conditions
-        $routeProvider.when('/get-myaat/aat-website-terms-and-conditions', {
-          pageTitle: 'Terms and conditions',
-          templateUrl: 'components/static-pages/terms.tpl.html',
-          controller: 'MyaatController',
-          controllerAs: 'vm',
-          access: {
-            requiresLogin: false,
-            roles: []
-          }
-        });
-
-        //Terms and conditions
-        $routeProvider.when('/get-myaat/help', {
-          pageTitle: 'Need help logging in to MyAAT?',
-          templateUrl: 'components/static-pages/help.tpl.html',
           controller: 'MyaatController',
           controllerAs: 'vm',
           access: {
