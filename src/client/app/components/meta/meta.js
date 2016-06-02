d0bc6d41c36cbffb3681d4e1efde0043aae6f869
Mark Rushton <mark@modernfidelity.co.uk>
2016-06-02 15:34:12 +0100

    started comments on meta

--- src/client/app/components/meta/meta.js
+++ src/client/app/components/meta/meta.js
@@ -1,8 +1,18 @@
 /**
- * Meta Module
- * @file Handles page meta data content i.e. MetaTags, PageTitles, OpenGraph, Twitter Cards etc...
+ *
+ * META COMPONENT
+ *
+ * @author David Breuer <David.Breuer@aat.org.uk>
+ * @author Mark Rushton <mark@modernfidelity.co.uk>
+ *
+ * @class app.Meta
+ *
+ * @description Handles page meta data content i.e. MetaTags, PageTitles, OpenGraph, Twitter Cards etc...
+ *
+ * @memberof app
  *
  */
+
 (function() {
 
   angular.module('metatags', [])
@@ -11,16 +21,35 @@
       var routes = {};
       var otherwise = {};
 
+      /**
+       * Check Path
+       *
+       * @param {string} path
+       * @param {object} metatags
+       * @returns {app.Meta}
+       */
       this.when = function(path, metatags) {
         routes[path] = metatags;
         return this;
       };
 
+      /**
+       * Defaults
+       *
+       * @param {object} metatags
+       * @returns {app.Meta}
+       */
       this.otherwise = function(metatags) {
         otherwise = metatags;
         return this;
       };
 
+      /**
+       * Get Metas
+       *
+       * @param {object} path
+       * @returns {{}}
+       */
       var getMetaTags = function(path) {
         var info = {};
         var routesArray = Object.keys(routes);
@@ -36,7 +65,7 @@
         var flag1;
         var flag2;
 
-        for (var i = 0; i < routesLength; i++) {
+        for(var i = 0; i < routesLength; i++) {
 
           routeName = routesArray[i];
           routeMetaTagsObject = routes[routeName];
@@ -52,7 +81,7 @@
             continue;
           }
 
-          for (var j = 0; j < pathArgsLength; j++) {
+          for(var j = 0; j < pathArgsLength; j++) {
             if (routeArgs[j].indexOf(':') === 0) {
               placeholder[pathArgs[j]] = routeArgs[j];
               continue;
@@ -68,7 +97,7 @@
           var placeHolderLength = Object.keys(placeholder).length;
 
           if (placeHolderLength > 0) {
-            for (var ii = 0; ii < routeMetaTagsLength; ii++) {
+            for(var ii = 0; ii < routeMetaTagsLength; ii++) {
               var tag = routeMetaTagsArray[ii];
               if (typeof(routeMetaTagsObject[tag]) === 'string') {
                 info[tag] = routeMetaTagsObject[tag];
@@ -83,14 +112,14 @@
               }
             }
 
-            for (var p in placeholder) {
-              for (var t in info) {
+            for(var p in placeholder) {
+              for(var t in info) {
                 info[t] = info[t].replace(placeholder[p], p);
               }
             }
             return info;
           } else {
-            for (var o in otherwise) {
+            for(var o in otherwise) {
               info[o] = otherwise[o];
             }
 
@@ -101,7 +130,7 @@
           }
         }
         if (flag1 && flag2) {
-          for (var r in routeMetaTagsObject) {
+          for(var r in routeMetaTagsObject) {
             info[r] = routeMetaTagsObject[r];
           }
           return info;
@@ -115,7 +144,7 @@
         var update = function() {
           var path = $location.path();
           var info = getMetaTags(path);
-          for (var tt in info) {
+          for(var tt in info) {
             $rootScope.metatags[tt] = info[tt];
           }
         };
@@ -126,7 +155,7 @@
             try {
               angular.module('ngRoute');
               $rootScope.$on('$routeChangeSuccess', update);
-            } catch (err) {
+            } catch(err) {
               $rootScope.$on('$stateChangeSuccess', update);
             }
           }
